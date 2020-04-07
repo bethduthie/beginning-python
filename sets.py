ce564f02dd07d336838d520c5b023d26550985a2
havelock <bethduthie@gmmail.com>
2020-04-07 18:10:03 +0000

    Started working on sets

---
+++ sets.py
@@ -0,0 +1,6 @@
+cs_courses = {'maths', 'history', 'pysics', 'compsci'}
+art_courses = {'maths', 'history', 'art', 'design'}
+
+# print(cs_courses.intersection(art_courses))
+# print(cs_courses.difference(art_courses))
+print(cs_courses.union(art_courses))
