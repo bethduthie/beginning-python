67822ced5a0acb58c1fb320259b9ff71809be9f5
havelock <bethduthie@gmmail.com>
2020-04-07 12:25:54 +0000

    Changed message, put it on two lines

--- intro.py
+++ intro.py
@@ -1,1 +1,9 @@
-print('Hello world')
+# be descriptive with titles, use underscores when using multiple words
+# ie my_message not my message
+# tripple " when going onto multiple lines
+
+message = """Bobby's world was a good
+cartoon in the 90s"""
+
+print(message)
+
