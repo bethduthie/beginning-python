ff431a951d70a564729f165012fa561b0f714cc3
havelock <bethduthie@gmmail.com>
2020-04-08 16:35:55 +0000

    Started examples for dictionaries, loops and iterations, and functions

---
+++ functions.py
@@ -0,0 +1,28 @@
+# DRY ('don't repeat yourself')
+
+
+# def hello_func(greeting, name='You'):
+# 	return '{}, {}'.format(greeting, name)
+
+
+# print(hello_func('Hi', name= 'James'))
+
+# example using calander and leap years
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def is_leap(year):
+	return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+	if not 1 <= month <= 12:
+		return 'Invaled Month'
+
+	if month == 2 and is_leap(year):
+		return 29
+
+	return month_days(month)
+
+print(days_in_month(2020, 2))
+
