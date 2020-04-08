ffa9f914032eef01122f4fc4cfa2c6a588f1e899
havelock <bethduthie@gmmail.com>
2020-04-08 14:29:55 +0000

    Started conditional and boolean examples

---
+++ conditionals.py
@@ -0,0 +1,34 @@
+# case sensitive
+
+# language = 'english'
+
+# if language == 'English':
+# 	print('language is English')
+# elif language == 'english':
+# 	print('language is English')
+# else:
+# 	print('language is not English')
+
+# can add multiple elif
+
+# user = 'admin'
+# logged_in = True
+
+# if user == 'admin' and logged_in:
+# 	print('admin page')
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+
+# print(a is b) #true
+# print(a is b) #false
+# # because they have equal value but take up different places in memory
+
+# False values:
+# False
+# None
+# Zero ie 0 when using numbers
+# Any empty sequence ex '' () {}
+# Any empty mapping ex []
+
+#everything else evaluates to True
