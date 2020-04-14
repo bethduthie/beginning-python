e051d0e2d8c597f52c3f7a527edc3fbcb3979bce
havelock <bethduthie@gmmail.com>
2020-04-14 13:13:20 +0000

    Added more notes to dictionary file

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,16 @@
-student = {'name': 'John', 'age': 25, 'courses': ['Maths', 'Pysics']}
+student = {'name': 'John', 'age': 25, 'courses': ['Maths', 'Physics']}
+
+# print(student.get('age'))
+
+# print(student.get('phone'))
+# student['phone'] = '01432 575757'
+# student.update({})
+# len(student)
+# print(student.keys())
+# print(student.values())
+# student.items()
+# del student['age']
+
 
 for keys, values in student.items():
-	print(keys, values)
+    print(keys, values)
